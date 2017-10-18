3c04a9e59d85329ea7f2cbd5c65512b5250d0ba3
Jason Scott <dev.jason.scott@gmail.com>
2017-10-18 20:57:41 +0200

    adds admin role

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -16,3 +16,37 @@ exports.profile = async (req, res) => {
       res.status(500).send({error: 'Bad request'});
   }
 };
+
+exports.delete = async (req, res) => {
+    console.log(req.session);
+    console.log(req.body);
+    if(!req.session || !req.session.user || !req.session.user.isAdmin) {
+        return res.status(400).send({message: 'Only an admin can perform this action'});
+    }
+
+    if(!req.body.userId) res.status(422).send({message: 'You must include a user ID'});
+    const userId = req.body.userId;
+    try {
+        const deletedUser = await UserModel.findByIdAndRemove(userId);
+        res.status(200).send({message: "User removed."});
+    } catch(error) {
+        console.warn(error);
+    }
+};
+
+exports.promote = async (req, res) => {
+    console.log(req.session);
+    console.log(req.body);
+    if(!req.session || !req.session.user || !req.session.user.isAdmin) {
+        return res.status(400).send({message: 'Only an admin can perform this action'});
+    }
+
+    if(!req.body.userId) res.status(422).send({message: 'You must include a user ID'});
+    const userId = req.body.userId;
+    try {
+        const promotedUser = await UserModel.findByIdAndUpdate(userId, {$set: {isAdmin: true}});
+        res.status(200).send({message: "User promoted."});
+    } catch(error) {
+        console.warn(error);
+    }
+};
