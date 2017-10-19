65790ec7e935191ac163b444d970c967ca5cea2d
Jason Scott <dev.jason.scott@gmail.com>
2017-10-19 21:30:58 +0200

    finishes for turn in

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,8 +1,7 @@
 const UserModel = require('../models/UserModel');
+const PostModel = require('../models/PostModel');
 
 exports.profile = async (req, res) => {
-    console.log(req.session);
-    console.log(req.query);
   if(!req.session.user) res.status(500).send({error: 'Unauthorized'});
   if(!req.query.userId) res.status(422).send({error: 'Request must include a user ID'});
 
@@ -10,7 +9,6 @@ exports.profile = async (req, res) => {
 
   try {
       const user = await UserModel.findById(userId);
-      console.log(user);
       res.status(200).send({user});
   } catch(err) {
       res.status(500).send({error: 'Bad request'});
@@ -18,16 +16,16 @@ exports.profile = async (req, res) => {
 };
 
 exports.delete = async (req, res) => {
-    console.log(req.session);
-    console.log(req.body);
     if(!req.session || !req.session.user || !req.session.user.isAdmin) {
         return res.status(400).send({message: 'Only an admin can perform this action'});
     }
 
-    if(!req.body.userId) res.status(422).send({message: 'You must include a user ID'});
-    const userId = req.body.userId;
+    if(!req.query.userId) res.status(422).send({message: 'You must include a user ID'});
+    const userId = req.query.userId;
     try {
-        const deletedUser = await UserModel.findByIdAndRemove(userId);
+        const userToDelete = await UserModel.findById(userId);
+        const deletedPosts = await PostModel.remove({author: userToDelete});
+        await userToDelete.remove();
         res.status(200).send({message: "User removed."});
     } catch(error) {
         console.warn(error);
@@ -35,14 +33,12 @@ exports.delete = async (req, res) => {
 };
 
 exports.promote = async (req, res) => {
-    console.log(req.session);
-    console.log(req.body);
     if(!req.session || !req.session.user || !req.session.user.isAdmin) {
         return res.status(400).send({message: 'Only an admin can perform this action'});
     }
 
-    if(!req.body.userId) res.status(422).send({message: 'You must include a user ID'});
-    const userId = req.body.userId;
+    if(!req.query.userId) res.status(422).send({message: 'You must include a user ID'});
+    const userId = req.query.userId;
     try {
         const promotedUser = await UserModel.findByIdAndUpdate(userId, {$set: {isAdmin: true}});
         res.status(200).send({message: "User promoted."});
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,4 +2,6 @@ const UserController = require('../controllers/UserController');
 
 module.exports = (app) => {
     app.get('/api/user/profile', UserController.profile);
+    app.delete('/api/user', UserController.delete);
+    app.patch('/api/user/promote', UserController.promote);
 };
